981349347a4ab2f9b6df2c927a884ae435b38b05
Collin Smith <collink33@gmail.com>
2015-09-15 15:18:45 -0700

    Add form

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -51,11 +51,24 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = React.findDOMNode(this.refs.author)
+    var text = React.findDOMNode(this.refs.text).value.trim();
+    if (!text || !author) {
+      return;
+    }
+    React.findDOMNode(this.refs.author).value = '';
+    React.findDOMNode(this.refs.text).value = '';
+  }
+
   render: function () {
     return (
-      <div className="commentForm">
-        Hello, world! I am a Comment Form.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something..." ref="text" />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 });
