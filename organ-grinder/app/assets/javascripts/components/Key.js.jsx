d1efbe9c87c29217ea9c832495d88a14e3d70ec8
Collin Smith <collink33@gmail.com>
2015-11-01 14:06:15 -0800

    Add styles based on keypress

--- organ-grinder/app/assets/javascripts/components/Key.js.jsx
+++ organ-grinder/app/assets/javascripts/components/Key.js.jsx
@@ -1,19 +1,32 @@
 var Key = React.createClass({
+  getInitialState: function () {
+    return { pressed: false };
+  },
+
   componentDidMount: function () {
     this.note = new Note(this.props.freq);
     KeyStore.addChangeListener(this._keyChange);
   },
 
   render: function () {
+    var style;
+    if (this.state.pressed) {
+      style = {
+        background: 'blue'
+      };
+    }
+
     return (
-      <div>{this.props.noteName}</div>
+      <div className="key" style={style}>{this.props.noteName}</div>
     )
   },
 
   _keyChange: function () {
     if (KeyStore.all().indexOf(this.props.noteName) !== -1) {
+      this.setState({ pressed: true });
       this.note.start();
     } else {
+      this.setState({ pressed: false });
       this.note.stop();
     }
   }
