e10c3577e8df19b190d3431254304ffbc7c67d82
Collin Smith <collin@collinsmith.me>
2018-05-22 13:26:02 -0500

    Add logging to store

--- todos-middleware/frontend/todo_redux.jsx
+++ todos-middleware/frontend/todo_redux.jsx
@@ -4,11 +4,22 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+function addLoggingtoDispatch(store) {
+  const dispatch = store.dispatch;
+
+  return function(action) {
+    console.log(store.getState());
+    console.log(action);
+    dispatch(action);
+    console.log(store.getState());
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
-
+  store.dispatch = addLoggingtoDispatch(store);
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
