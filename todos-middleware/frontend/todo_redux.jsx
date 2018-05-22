31a2d83d9de99153d07761a24aacf6ff2591cacd
Collin Smith <collin@collinsmith.me>
2018-05-22 13:42:10 -0500

    Use builtin redux applyMiddleware

--- todos-middleware/frontend/todo_redux.jsx
+++ todos-middleware/frontend/todo_redux.jsx
@@ -4,29 +4,10 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
-const addLoggingToDispatch = store => next => action => {
-  console.log(store.getState());
-  console.log(action);
-  next(action);
-  console.log(store.getState());
-}
-
-function applyMiddlewares(store, middlewares) {
-  let dispatch = store.dispatch;
-
-  middlewares.forEach(middleware => {
-    dispatch = middleware(store)(dispatch)
-  });
-
-  return Object.assign({}, store, { dispatch });
-}
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  let store = configureStore(preloadedState);
-  store = applyMiddlewares(store, [addLoggingToDispatch]);
+  const store = configureStore(preloadedState);
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
