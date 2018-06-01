9f87670991e7b17750e61a84264d096357a894a6
Collin Smith <collink33@gmail.com>
2018-05-31 19:44:24 -0500

    Add ability to submit search

--- giphy-search/components/giphys_search.jsx
+++ giphy-search/components/giphys_search.jsx
@@ -1,3 +1,42 @@
 import React from 'react';
 
 import GiphysIndex from './giphys_index';
+
+export default class GiphysSearch extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchTerm: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ searchTerm: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.fetchSearchGiphys(this.state.searchTerm);
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.searchTerm}
+            onChange={this.handleChange}
+          />
+          <button type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
